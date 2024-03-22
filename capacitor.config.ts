4dcd57a06107993e1337e7149b874dc18b0d835a
unknown <andreyansyah737@gmail.com>
2024-03-22 11:31:06 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'helios.10th.anniversary',
+  appName: 'helios-10th-anniversary',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
